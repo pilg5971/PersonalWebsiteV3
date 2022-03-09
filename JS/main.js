df4356219bae107c74068d5a08befda73c530998
Trevor Pilgrim <tpilgrim@fishbowlsolutions.com>
2022-03-09 15:06:15 -0600

    Updated JS Function Name

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-// <!-- ======================== ABOUT IMAGES ======================== -->
+// <!-- ======================== ABOUT FLEX CARDS ======================== -->
 $(".option").click(function(){
   $(".option").removeClass("active");
   $(this).addClass("active");
