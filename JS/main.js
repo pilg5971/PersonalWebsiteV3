08cb25d3077d1845201ed891c44e6598e4ffd9d3
Trevor Pilgrim <tpilgrim@fishbowlsolutions.com>
2022-03-22 13:33:19 -0500

    Commented CK Code

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-// <!-- ======================== ABOUT FLEX CARDS ======================== -->
+// <!-- ======================== SKILL FLEX CARDS ======================== -->
 $(".flex__card").click(function(){
   $(".flex__card").removeClass("active");
   $(this).addClass("active");
